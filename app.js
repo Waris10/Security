a85355f2266051aa09096f605c1c42a25bb59f11
Waris10 <alaseayomide10@gmail.com>
2022-10-24 16:25:49 +0100

    I hashed my users password

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({
@@ -24,67 +25,61 @@ const userSchema = new mongoose.Schema(
     password : String
   });
 
-  const secret = process.env.SECRET
-  userSchema.plugin(encrypt,
-    {
-      secret: secret,
-      encryptedFields: ['password']
+  const User = mongoose.model('User', userSchema);
+
+  // TODO:
+
+  app.get('/', (req, res) =>{
+    res.render('home');
+  });
+
+  app.get('/login', (req, res) =>{
+
+    res.render('login');
+  });
+
+  app.get('/register', (req, res) =>{
+    res.render('register');
+  });
+
+  app.post('/register', (req, res) =>{
+    const hashedPassword = md5(req.body.password)
+    const newUser = new User({
+      email : req.body.username,
+      password : hashedPassword
     });
 
-    const User = mongoose.model('User', userSchema);
-
-    // TODO:
-
-    app.get('/', (req, res) =>{
-      res.render('home');
+    newUser.save((err) =>{
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets');
+      }
     });
+  });
 
-    app.get('/login', (req, res) =>{
+  app.post('/login', (req, res) =>{
+    const username = req.body.username;
+    const password = md5(req.body.password);
 
-      res.render('login');
-    });
-
-    app.get('/register', (req, res) =>{
-      res.render('register');
-    });
-
-    app.post('/register', (req, res) =>{
-      const newUser = new User({
-        email : req.body.username,
-        password : req.body.password
-      });
-
-      newUser.save((err) =>{
-        if (err) {
-          console.log(err);
-        } else {
-          res.render('secrets');
-        }
-      });
-    });
-
-    app.post('/login', (req, res) =>{
-      const username = req.body.username;
-      const password = req.body.password;
-
-      User.findOne({email : username}, (err, foundUser) =>{
-        if (err) {
-          console.log(err);
-        } else {
-          if (foundUser) {
-            if (foundUser.password === password ) {
-              res.render('secrets');
-            }
+    User.findOne({email : username}, (err, foundUser) =>{
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          if (foundUser.password === password ) {
+            res.render('secrets');
           }
         }
-      });
+      }
     });
+  });
 
 
 
 
 
 
-    app.listen(3000, () => {
-      console.log('Server has started successfully');
-    })
+  app.listen(3000, () => {
+    console.log('Server has started successfully');
+  })
